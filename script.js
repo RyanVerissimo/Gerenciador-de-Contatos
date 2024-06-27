49bc6e3cb8f63fe1ef50c804e58f53fd020e74e6
Ryan Verissimo de Oliveira <ryanvm.black@gmail.com>
2024-06-27 17:49:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let contacts = [];
 let selectedContactIndex;
 
+document.getElementById('toggle-theme-btn').addEventListener('click', function() {
+    const body = document.body;
+    const currentTheme = body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme';
+    
+    if (currentTheme === 'dark-theme') {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        document.getElementById('toggle-theme-btn').innerHTML = '<i class="ph ph-moon"></i>';
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        document.getElementById('toggle-theme-btn').innerHTML = '<i class="ph ph-sun"></i>';
+    }
+});
+
 function addContact(event) {
     event.preventDefault();
     const form = event.target;
@@ -18,14 +33,54 @@ function addContact(event) {
     }
 }
 
+// function displayContacts() {
+//     const contactsList = document.getElementById('contacts-list');
+//     contactsList.innerHTML = '';
+//     contacts.forEach((contact, index) => {
+//         const listItem = document.createElement('li');
+//         listItem.classList.add('contact-item');
+//         listItem.innerHTML = `
+//             <div class="contact-info">
+//                 <h3>${contact.name}</h3>
+//                 <button class="favorite-button" style="width: 40px;" onclick="toggleFavorite(${index})">
+//                     <i class="ph ph-heart-straight""></i>
+//                 </button>
+//             </div>
+//             <p>Telefone: ${contact.phone}</p>
+//             <p>E-mail: ${contact.email}</p>
+//             <div class="contact-buttons">
+//                 <button onclick="openEditModal(${index})">Editar</button>
+//                 <button onclick="deleteContact(${index})">Excluir</button>
+//             </div>
+//         `;
+//         contactsList.appendChild(listItem);
+//     });
+// }
+
+function toggleFavorite(index) {
+    contacts[index].favorite = !contacts[index].favorite; // Alternar o estado de favorito
+
+    displayContacts(); // Atualizar a exibição dos contatos
+}
+
+
 function displayContacts() {
     const contactsList = document.getElementById('contacts-list');
     contactsList.innerHTML = '';
+
     contacts.forEach((contact, index) => {
         const listItem = document.createElement('li');
         listItem.classList.add('contact-item');
+
+        const favoriteClass = contact.favorite ? 'favorite' : ''; // Verifica se é favorito
+
         listItem.innerHTML = `
-            <h3>${contact.name}</h3>
+            <div class="contact-info">
+                <h3>${contact.name}</h3>
+                <button class="favorite-button ${favoriteClass}" style="width: 40px;" onclick="toggleFavorite(${index})">
+                    <i class="ph ph-heart-straight"></i>
+                </button>
+            </div>
             <p>Telefone: ${contact.phone}</p>
             <p>E-mail: ${contact.email}</p>
             <div class="contact-buttons">
@@ -33,9 +88,13 @@ function displayContacts() {
                 <button onclick="deleteContact(${index})">Excluir</button>
             </div>
         `;
+
         contactsList.appendChild(listItem);
     });
 }
+
+
+
 
 function clearForm() {
     document.getElementById('contact-form').reset();
@@ -83,5 +142,4 @@ displayContacts();
 document.getElementById('contact-form').addEventListener('submit', addContact);
 document.getElementById('edit-modal-close').addEventListener('click', closeEditModal);
 document.getElementById('edit-modal-save').addEventListener('click', saveContact);
-
 
